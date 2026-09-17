a1d251060b97a9fcc79900c436f023e939b99841
lucasdias0259 <dlucasmd9@gmail.com>
2026-09-17 14:26:31 -0300

    Listas Nivel 1

---
+++ Listas NÍVEL 1 - Iniciante/ex02/ex02.py
@@ -0,0 +1,14 @@
+"""
+Crie uma lista com números de 1 a 10 e:
+
+Imprima quantos elementos tem
+Imprima o dobro de elementos
+
+"""
+numeros = [1,2,3,4,5,6,7,8,9,10]
+
+print(f"=== A LISTA TEM {len(numeros)} ELEMENTOS ===")
+print(f"=== O DOBRO DE ELEMTENTOS DA LISTA É {len(numeros*2)} ===\n")
+
+print(len(numeros))
+print(len(numeros*2))
---
+++ Listas NÍVEL 1 - Iniciante/ex03/ex03.py
@@ -0,0 +1,21 @@
+"""
+Exercício 1.3: Adicionar Elementos
+
+Comece com uma lista vazia e:
+
+1.Adicione 3 números inteiros
+2.Imprima a lista
+3.Adicione mais 2 números
+4.Imprima a lista novamente
+"""
+
+lista_numeros = []
+lista_numeros.append(1)
+lista_numeros.append(2)
+lista_numeros.append(3)
+
+print(lista_numeros)
+
+lista_numeros.append(4)
+lista_numeros.append(5)
+print(lista_numeros)
